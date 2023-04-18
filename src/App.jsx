e8c2e811168d76c932ab9de4473377720b23a2c2
Aman Singh Bhogal <79533183+asbhogal@users.noreply.github.com>
2023-04-18 17:47:19 +0100

    refactored code and improvements to UI

--- src/App.jsx
+++ src/App.jsx
@@ -17,8 +17,8 @@ const App = () => {
       id: 1,
       name: "username",
       type: "text",
-      placeholder: "Username",
-      label: "Username",
+      placeholder: "Username*",
+      label: "Username*",
       errorMessage: "Please enter a username between 3-16 characters with no spaces or special characters",
       required: true,
       pattern: "^[a-zA-Z0-9]{3,16}$"
@@ -27,8 +27,8 @@ const App = () => {
       id: 2,
       name: "emailAddress",
       type: "text",
-      placeholder: "Email Address",
-      label: "Email Address",
+      placeholder: "Email Address*",
+      label: "Email Address*",
       errorMessage: "Please enter an email address in a valid format",
       required: true,
       pattern: "^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$"
@@ -45,23 +45,23 @@ const App = () => {
       id: 4,
       name: "password",
       type: "text",
-      placeholder: "Password",
-      label: "Password",
+      placeholder: "Password*",
+      label: "Password*",
       errorMessage: "Please enter a password between 8 - 64 characters in length, with at least 1 uppercase, 1 lowercase, 1 number and a special symbol (e.g. */£!)",
       required: true,
-      pattern: "^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[!@#$%^&*()_+])[A-Za-z\d!@#$%^&*()_+]{8,65}$"
+      pattern: "/^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$%^&*-]).{8,}$/"
     },
     {
       id: 5,
       name: "repeatPassword",
       type: "text",
-      placeholder: "Repeat Password",
-      label: "Repeat Password",
+      placeholder: "Repeat Password*",
+      label: "Repeat Password*",
       errorMessage: "Please make sure the password entered matches the password above",
       required: true,
       pattern: values.password
     }
-]
+  ]
 
   const handleSubmit = (e) => {
     e.preventDefault();
@@ -71,27 +71,25 @@ const App = () => {
     setValues({ ...values, [e.target.name]: e.target.value })
   }
 
-  console.log(values);
-
   return (
     <>
       <h1>React Form Validation</h1>
 
-      <form onSubmit={ handleSubmit }>
-        { inputs.map((input) => (
-          <FormInput 
-            key={ input.id } 
-            {...input} 
-            value={ values[input.name] }
-            onChange={ onChange }
-            />
+      <form onSubmit={handleSubmit}>
+        {inputs.map((input) => (
+          <FormInput
+            key={input.id}
+            {...input}
+            value={values[input.name]}
+            onChange={onChange}
+          />
         ))}
         <button>SUBMIT</button>
       </form>
 
       <span className='warning-message'>THIS FORM IS FOR FRONT-END DEMONSTRATION PURPOSES ONLY. PLEASE DO NOT ENTER IN VALID, IDENTIFIABLE DATA AS ANY INFORMATION ENTERED IS NOT ENCRYPTED</span>
     </>
-    
+
   )
 }
 
--- src/components/FormInput.jsx
+++ src/components/FormInput.jsx
@@ -1,29 +1,25 @@
 import { useState } from "react";
 
-const FormInput = (props) => {
-
-  const { id, label, errorMessage, onChange, ...inputProps } = props;
-
+const FormInput = ({ id, label, errorMessage, onChange, ...inputProps }) => {
   const [focused, setFocused] = useState(false);
 
-  const handleFocused = (e) => {
+  const handleFocused = () => {
     setFocused(true);
-  }
+  };
 
   return (
-    
     <div className="form-input">
-        <label>{ label }</label>
-        <input
-            { ...inputProps }
-            onChange={ onChange }
-            onBlur={ handleFocused }
-            focused={ focused.toString() }
-            onFocus={ () => inputProps.name === "repeatPassword" && setFocused(true)}
-            />
-        <span className="error-message">{ errorMessage }</span>
+      <label>{label}</label>
+      <input
+        {...inputProps}
+        onChange={onChange}
+        onBlur={handleFocused}
+        focused={focused.toString()}
+        onFocus={() => inputProps.name === "repeatPassword" && setFocused(true)}
+      />
+      <span className="error-message">{errorMessage}</span>
     </div>
-  )
-}
+  );
+};
 
-export default FormInput
+export default FormInput;
